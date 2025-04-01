c4aba62f37c0221ce13279e69821e7e1b97da039
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2025-04-01 15:34:17 -0300

    Update B-09.s

--- B-09.s
+++ B-09.s
@@ -1,32 +1,23 @@
-# B.09 Escriba un programa llamado “ITOAREVERSE.s”. El mismo debe grabar en el registro x5
-# el valor decimal 1234. Luego debe dividir el mismo por 10 (utilizando el método de restassucesivas de B.07), mostrar el valor del resto por consola
-# (recuerde que el ASCII del carácter 0 es 0x30 y el 9 es 0x39), y almacenar el resultado nuevamente en x5. Continuar ejecutando
-# esta secuencia hasta que x5 sea cero
+# B.08 Escriba un programa que imprima por consola el valor de un dato (32 bits signado)
+# almacenado en memoria. Complete el programa a partir de la sección text.
+#.data es el espacio de memoria para variables, asi como .text es el espacio de memoria para el codigo
+# la rd,symbol = pseudoinstruccion que carga lo que se encuentra dentro de la direccion indicada en symbol en el registro destino rd
+
+#dword = palabra de 64 bits
+#word = palabra entera de 32 bits
+#half = media palabra, 16 bits
+#byte = 1/4 de palabra, 8bits
 
 
+.data
+variable: .half 32769 #65535
 .text
-    li x5, 12345
-    li x6, 10
-    li x7, 0 # Contador de divisiones
-    
-dividir:
-restar:
-    sub x5, x5, x6 # resto al dividendo el divisor
-    addi x7, x7, 1
-    bge x5, x6, restar
-    
-    blt x5, x0, descarta_ultima_resta
-    bge x5, x0, fin
-descarta_ultima_resta:
-    add x5, x5, x6
-    addi x7, x7, -1
 fin:
-    li a7, 1
-    add a0, x0, x5
-    ecall
-    
-    add x5, x0, x7
-    li x7, 0
-    bne x5, x0, dividir
-    
-    nop
+  addi x17,x0 1
+  lh x10,variable
+  ecall
+  addi x17,x0 10
+  ecall  
+  
+
+#load half carga la parte alta con 0xffff

7d4d732407add353c627bb53ad7530130cef7a41
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2025-04-01 15:41:36 -0300

    Update B-10.s

--- B-10.s
+++ B-10.s
@@ -1,29 +1,20 @@
-# B.10 Escriba un programa llamado “DIV8.s”. El mismo debe tomar un valor en el registro x5 y
-# dividirlo por 8. NO se puede usar el método de restas sucesivas, y el mismo debe funcionar
-# para valores positivos o negativos. Ej: si x5 = -25, el resultado es -3. Repase la instrucción
-# SRAI.
+# B.10 Escriba un programa que imprima por consola el valor dado (16 bits sin signo)
+# almacenado en memoria.
+#.data es el espacio de memoria para variables, asi como .text es el espacio de memoria para el codigo
+# la rd,symbol = pseudoinstruccion que carga lo que se encuentra dentro de la direccion indicada en symbol en el registro destino rd
 
-# Al hacer SRAI, si el valor original es de signo negativo e impar (bms == 1)
-# debemos restar uno al resultado
+#dword = palabra de 64 bits
+#word = palabra entera de 32 bits
+#half = media palabra, 16 bits
+#byte = 1/4 de palabra, 8bits
 
+
+.data
+variable: .half 32769 #65535
 .text
-    li x5,-25
-    add x7, x0, x5
-    srai x5, x5, 3
-    
-    bge x5 x0, fin
-    blt x5, x0, verificar_resto_0
-verificar_resto_0:
-    andi x8, x7, 7
-    bne x8, x0, corregir_division
-    beq x8, x0, fin
-    
-corregir_division:
-    addi x5, x5, 1
-    
 fin:
-    addi a7, x0, 1
-    add a0, x0, x5
-    ecall
-    
-    nop
+  addi x17,x0 36
+  lw x10,variable
+  ecall
+  addi x17,x0 10
+  ecall  

57062928ef3e3921e165edc01fb3b44e6fb3df40
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2025-04-01 15:18:02 -0300

    Update B-08.s

--- B-08.s
+++ B-08.s
@@ -1,17 +1,15 @@
-#B.08 Imprimir 5678 consideranco los caracteres ASCII.
-text
-    addi a0,x0,0x35
-    addi a7,x0,11
-    ecall
-    
-    addi a0,x0,0x36
-    addi a7,x0,11
-    ecall
-    
-    addi a0,x0,0x37
-    addi a7,x0,11
-    ecall
-    
-    addi a0,x0,0x38
-    addi a7,x0,11
-    ecall
+# B.08 Escriba un programa que imprima por consola el valor de un dato (32 bits signado)
+# almacenado en memoria. Complete el programa a partir de la sección text.
+#.data es el espacio de memoria para variables, asi como .text es el espacio de memoria para el codigo
+# la rd,symbol = pseudoinstruccion que carga lo que se encuentra dentro de la direccion indicada en symbol en el registro destino rd
+
+.data
+variable: .word 123456
+
+.text
+fin:
+  addi x17,x0 1
+  la x10,variable
+  ecall
+  addi x17,x0 10
+  ecall

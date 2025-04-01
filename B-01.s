94da57aa61af4783443e180a49a7e800d6d2f06a
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2025-04-01 14:15:39 -0300

    Update B-01.s

--- B-01.s
+++ B-01.s
@@ -1,2 +1,9 @@
+#B.01 Escriba la instrucción necesaria en Ripes para lograr que el registro x5 tenga el valor
+#hexadecimal 0x000003FF (note que 3FF se codifica con menos de 12 bits)
+#En el área de registros (seleccionado display type Signed), luego de ejecutar la instrucción,
+#debe aparecer en X5:
+
 .text
+fin:
     li x5,0x03FF
+    bge x0 x0 fin

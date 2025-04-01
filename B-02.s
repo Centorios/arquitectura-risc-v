f51959b1dc6fda2aa549e7bbcfcaf32e30d00e4d
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2025-04-01 14:29:46 -0300

    Update B-02.s

--- B-02.s
+++ B-02.s
@@ -1,5 +1,6 @@
-#B.02 Escriba las instrucciones necesarias en Ripes para lograr que el registro x5 tenga el
-#valor hexadecimal 0x12345678
+# B.02 Escriba las instrucciones necesarias en Ripes para lograr que el registro x5 tenga el
+# valor hexadecimal 0x12345678
+
 .text
 fin:
   lui x5, 0x12345

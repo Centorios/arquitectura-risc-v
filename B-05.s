760dab02d37c761701d3fd0d694f8ba183f7daa6
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2025-04-01 14:41:38 -0300

    Update B-05.s

--- B-05.s
+++ B-05.s
@@ -1,21 +1,15 @@
-# Escriba el valor 100 en x7 y luego programe un bucle utilizando el registro x8 como
-# contador de 10 a 0. En cada iteración del bucle debe incrementar en uno el valor de x7. Indique
-# con qué valor comienza x7 y en qué valor termina luego del bucle.
+# B.04 Utilizando la system call ‘PrintInt’ (a7=1) imprima por consola el valor 1234.
+# a7 maneja que sucede con la ecall
+# a0 es el registro que se usa para lo que sea que haga la ecall
+# B.05 Modifique el programa anterior para que imprima -1234 y luego el programa termine
+# (Syscall Exit A7=10).
+    
 .text
-    addi x7, x0, 100
-    addi x8, x0, 10
-    
-    addi a7, x0, 1
-    
-loop:
-    addi, x7, x7, -1
-    addi x8, x8, -1
-    
-    add a0, x0, x8
-    ecall
-    
-    add a0, x0, x7
-    ecall
-    
-    bne x8, x0, loop
-    nop
+fin:
+  addi x17,x0 1 #addi a7,x0 1
+  addi x10,x0 -1234 #addi a0,x0 -1234
+  ecall
+  addi x17,x0 10
+  ecall
+
+bge x0 x0 fin

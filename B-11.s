3374acb98dad78222839aeceded80c8ecaa5307c
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2025-04-01 15:43:57 -0300

    Update B-11.s

--- B-11.s
+++ B-11.s
@@ -1,58 +1,19 @@
-# B.11 Escriba el programa “ITOA1024.s” basado en el punto B.09, de forma tal que el número
-# impreso por consola sea “1234” y no “4321”. Recomendamos almacenar cada dígito en
-# memoria e ir recorriendo la memoria en orden inverso para imprimir por consola. Puede crear
-# en memoria .data una variable de 4 bytes que almacene ceros inicialmente y utilizar este
-# espacio de memoria para invertir el número.
+# B.11 Escriba un programa que imprima por consola el valor dado (8 bits signado)
+# almacenado en memoria
+#.data es el espacio de memoria para variables, asi como .text es el espacio de memoria para el codigo
+# la rd,symbol = pseudoinstruccion que carga lo que se encuentra dentro de la direccion indicada en symbol en el registro destino rd
+
+#dword = palabra de 64 bits
+#word = palabra entera de 32 bits
+#half = media palabra, 16 bits
+#byte = 1/4 de palabra, 8bits
 
 .data
-    aux: .word 0
+variable: .byte 129
 .text
-    li x5, 1234
-    li x6, 10
-    li x7, 0 # Contador de divisiones
-    la x8, aux
-    
-dividir:
-restar:
-    sub x5, x5, x6 # resto al dividendo el divisor
-    addi x7, x7, 1
-    bge x5, x6, restar
-    
-    blt x5, x0, descarta_ultima_resta
-    bge x5, x0, fin
-descarta_ultima_resta:
-    add x5, x5, x6
-    addi x7, x7, -1
 fin:
-    # --- Muestra al derecho
-    # li a7, 1
-    # add a0, x0, x5
-    # ecall
-    
-    #Almaceno en aux para mostrar invertido
-    sb x5, 0(x8)
-    addi x8, x8, 1
-    
-    add x5, x0, x7
-    li x7, 0
-    
-    bne x5, x0, dividir
-    
-    #recorro en inverso x8
-    addi x8, x8, -1 # resto un byte de direccion
-    addi x7, x0, 4  # inicializo contador en 4
-
-recorrer_inverso:
-    lb x6, 0(x8) # Leo un digito 
-    li a7, 1 # funcion imprimir
-    add a0, x0, x6 # valor a imprimir
-    ecall
-        
-    addi x8, x8, -1 # resto un byte de direccion
-    addi x7, x7, -1 # resto uno al contador de iteraciones
-    
-    bne x7, x0, recorrer_inverso
-    beq x7, x0, fin_programa
- 
- fin_programa:   
-    nop
+  addi x17,x0 1
+  lb x10,variable
+  ecall
+  addi x17,x0 10
+  ecall  

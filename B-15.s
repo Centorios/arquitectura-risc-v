bbf1ec76802f9f510fde80657f708327c3929f8f
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2025-04-08 14:20:23 -0300

    Update B-15.s

--- B-15.s
+++ B-15.s
@@ -1,25 +1,21 @@
-# B.15 Se define un vector de 10 elementos (cada uno de 32 bits) llamado centena de la
-# siguiente forma:
-# .data
-# centena: .word 0,100,200,300,400,500,600,700,800,900
-# .text
-# la x5,centena
-# addi x6,x0,8
-# Dado que x5 posee la dirección de comienzo del vector, y x6=8, escriba las instrucciones
-# necesarias para que x7 obtenga el número 800 (almacenado en el vector). Recuerde que el
-# direccionamiento en RISC-V es al byte, por ende la palabra 8 de un vector se encuentra
-# almacenada en la dirección 8*4 relativa al comienzo del vector.
+# B.15 Repita los últimos dos ejercicios, pero el vector se encuentra compuesto de half-words
+# (16 bits).
 
 .data
-    centena: .word 0,100,200,300,400,500,600,700,800,900
+var: .half 100,1000,6666,655,1000,0
 .text
-    la x5,centena # Vector
-    addi x6,x0,8 # Indice del vector
-    
-    slli x6, x6, 2 # Multiplico por 4 para que el movimiento sea de words
-    add x6, x5, x6 # Sumo 8*4  a la direccion
-    
-    lw a0, 0(x6) # Cargo el valor del elem
-    li a7, 1 
-    ecall
-    
+
+addi a0,x0 0
+la x2,var
+salto:
+lh x1,0,x2
+addi x2,x2,2
+addi a0,a0,1
+bne x1,x0,salto
+
+
+addi a0,a0,-1
+addi x17,x0 1
+ecall
+addi x17,x0 10
+ecall 

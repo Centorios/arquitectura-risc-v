91141e5986c702b5cc9381ba205822e2fdf793bf
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2025-04-08 14:21:59 -0300

    Update B-15.s

--- B-15.s
+++ B-15.s
@@ -19,3 +19,26 @@ addi x17,x0 1
 ecall
 addi x17,x0 10
 ecall 
+
+
+# B.13 Escriba un programa que cuente cuantos elementos componen un vector utilizando cero
+# como último elemento del vector.
+
+#.data
+#var: .half 100,1000,6666,6666,7777,0
+#finvar:
+#.text
+
+#addi a0,x0 0
+#la x2,var
+#la x3,finvar
+#salto:
+#lw x1,0,x2
+#addi x2,x2,2
+#addi a0,a0,1
+#bne x2,x3,salto
+
+#addi x17,x0 1
+#ecall
+#addi x17,x0 10
+#ecall 
